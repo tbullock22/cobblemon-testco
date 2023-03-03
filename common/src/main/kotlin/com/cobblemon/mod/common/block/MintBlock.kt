7c98c6d066ce573e331d80b9f04c60fa5cc1b562
landonjw <landonjwdev@gmail.com>
2023-03-02 21:28:12 -0500

    Add license to new classes

--- common/src/main/kotlin/com/cobblemon/mod/common/block/MintBlock.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/block/MintBlock.kt
@@ -1,38 +1,22 @@
+/*
+ * Copyright (C) 2023 Cobblemon Contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.cobblemon.mod.common.block
 
 import dev.architectury.registry.registries.RegistrySupplier
-import net.minecraft.block.BlockState
 import net.minecraft.block.CropBlock
-import net.minecraft.block.ShapeContext
 import net.minecraft.item.BlockItem
 import net.minecraft.item.ItemConvertible
-import net.minecraft.util.math.BlockPos
-import net.minecraft.util.shape.VoxelShape
-import net.minecraft.world.BlockView
 
 class MintBlock(val seed: RegistrySupplier<BlockItem>, settings: Settings) : CropBlock(settings) {
-
-    private val AGE_TO_SHAPE = arrayOf(
-        createCuboidShape(0.0, 0.0, 0.0, 16.0, 2.0, 16.0),
-        createCuboidShape(0.0, 0.0, 0.0, 16.0, 3.0, 16.0),
-        createCuboidShape(0.0, 0.0, 0.0, 16.0, 4.0, 16.0),
-        createCuboidShape(0.0, 0.0, 0.0, 16.0, 5.0, 16.0),
-        createCuboidShape(0.0, 0.0, 0.0, 16.0, 6.0, 16.0),
-        createCuboidShape(0.0, 0.0, 0.0, 16.0, 7.0, 16.0),
-        createCuboidShape(0.0, 0.0, 0.0, 16.0, 8.0, 16.0),
-        createCuboidShape(0.0, 0.0, 0.0, 16.0, 9.0, 16.0)
-    )
 
     override fun getSeedsItem(): ItemConvertible {
         return seed.get()
     }
 
-    override fun getOutlineShape(
-        state: BlockState?,
-        world: BlockView?,
-        pos: BlockPos?,
-        context: ShapeContext?
-    ): VoxelShape {
-        return AGE_TO_SHAPE[(state!!.get(this.ageProperty) as Int)]
-    }
 }
